d9bb603fcf2acd7ad9648ed31bed822d48852f48
windeesel <windeesel365@gmail.com>
2024-04-22 20:58:47 +0700

    feature/story-1: Adding TaxRequest struct

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
 	"net/http"
+
+	_ "github.com/lib/pq"
+
+	"github.com/labstack/echo/v4"
 )
+
+// data structure pattern ที่ user client request
+type TaxRequest struct {
+	TotalIncome float64 `json:"totalIncome"`
+	WHT         float64 `json:"wht"`
+	Allowances  []struct {
+		AllowanceType string  `json:"allowanceType"`
+		Amount        float64 `json:"amount"`
+	} `json:"allowances"`
+}
 
 func main() {
 	e := echo.New()
