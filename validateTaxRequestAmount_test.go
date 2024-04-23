7ed5e62d46bbd80bad26e287f181d966c4e0228e
windeesel <windeesel365@gmail.com>
2024-04-23 16:33:44 +0700

    feature/story-3: update validateTaxRequestAmount and update test

--- validateTaxRequestAmount_test.go
+++ validateTaxRequestAmount_test.go
@@ -38,6 +38,49 @@ func TestValidateTaxRequestAmount(t *testing.T) {
 			},
 			wantErr: true,
 		},
+		{
+			name: "Zero total income",
+			req: TaxRequest{
+				TotalIncome: 0,
+				WHT:         10000,
+				Allowances: []struct {
+					AllowanceType string  `json:"allowanceType"`
+					Amount        float64 `json:"amount"`
+				}{
+					{AllowanceType: "donation", Amount: 300},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "Negative WHT",
+			req: TaxRequest{
+				TotalIncome: 50000,
+				WHT:         -10000,
+				Allowances: []struct {
+					AllowanceType string  `json:"allowanceType"`
+					Amount        float64 `json:"amount"`
+				}{
+					{AllowanceType: "donation", Amount: 300},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "Multiple allowances",
+			req: TaxRequest{
+				TotalIncome: 75000,
+				WHT:         15000,
+				Allowances: []struct {
+					AllowanceType string  `json:"allowanceType"`
+					Amount        float64 `json:"amount"`
+				}{
+					{AllowanceType: "donation", Amount: 500},
+					{AllowanceType: "k-receipt", Amount: 1000},
+				},
+			},
+			wantErr: false,
+		},
 	}
 
 	for _, tt := range tests {
